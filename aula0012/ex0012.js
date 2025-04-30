981614bad7dcc0e968b93f6c309e8730049799e9
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-04-29 22:20:16 -0300

    update

--- aula0012/ex0012.js
+++ aula0012/ex0012.js
@@ -1,10 +1,9 @@
 var agora = new Date()
 var hora = agora.getHours()
 
-
 console.log(`Agora são exatamente ${hora} horas.`)
 if (hora >= 0 && hora < 6) {
-    console.log(`Bom Madrugada!`)
+    console.log(`Boa Madrugada!`)
 } else if (hora < 12)  {
     console.log('Boa Dia!')
 } else  if (hora < 18) {
