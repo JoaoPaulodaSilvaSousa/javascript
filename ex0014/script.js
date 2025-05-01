d2cc4ff6908908c8b268fdb708fff4feb246287e
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-05-01 19:15:41 -0300

    update ex0014

--- ex0014/script.js
+++ ex0014/script.js
@@ -3,12 +3,20 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+
+    var hora = 20
+
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = 'Imagens/manha.png'
+        document.body.style.background = '#fde784'
     } else if (hora >= 12 && hora < 18){
         img.src = 'Imagens/tarde.png'
+        document.body.style.background = '#f6bc54'
     } else {
         img.src = 'Imagens/noite.png'
+        document.body.style.background = '#151924'
     }
 }
+
+
