51de78eb17ddab03fdb9c12478543416e5e0bfe4
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-05-11 13:55:23 -0300

    update aula14x

--- aula14ex/ex0017/script.js
+++ aula14ex/ex0017/script.js
@@ -1,1 +1,10 @@
-alert('Olá')
+function tabuada() {
+    let num = document.getElementById('numero')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+       let n = Number(num.value) 
+    }
+
+}
