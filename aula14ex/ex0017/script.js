bc709351c4d70e8af4bbfb080112784959ff942a
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-05-12 19:17:26 -0300

    update aula14/ex0017

--- aula14ex/ex0017/script.js
+++ aula14ex/ex0017/script.js
@@ -1,10 +1,14 @@
 function tabuada() {
-    let num = document.getElementById('numero')
-    let tab = document.getElementById('seltab')
-    if (num.value.length == 0) {
+    let num = document.getElementById('txtn')
+    let tabu = document.getElementById('seltab')
+
+    if ( num.value.length == 0) {
         window.alert('Por favor, digite um número!')
     } else {
-       let n = Number(num.value) 
+        let n = Number(num.value)
+        let c = 1
+        for (c = 1; c <= 10; c = n++) {
+            window.alert(`${c}`)
+        }
     }
-
 }
