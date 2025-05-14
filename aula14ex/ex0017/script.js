c5c0019b13c133ec930ad9d3b7dc8d86cc944eb5
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-05-14 20:25:08 -0300

    update aula14ex/ex0017

--- aula14ex/ex0017/script.js
+++ aula14ex/ex0017/script.js
@@ -9,11 +9,12 @@ function tabuada() {
     } else {
         let n = Number(num.value)
         let c = 1
-        for (c = 1; c <= 10; c = c++) {
+        for (c = 1; c <= 10; c++) {
             let item = document.createElement('option'); // Cria um elemento <option>
             item.text = `${n} x ${c} = ${n * c}`; // Define o texto da opção
             item.value = `tabu${c}`; // Define o valor da opção (opcional, mas útil)
             tabu.appendChild(item); // Adiciona a opção ao <select>
+            
         }
     }
 }
