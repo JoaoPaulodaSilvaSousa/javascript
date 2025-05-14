145e7ba9fb9d8fc9f8f2dc8b751113d8a0dcffe7
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-05-14 18:26:34 -0300

    update aulaex0017

--- aula14ex/ex0017/script.js
+++ aula14ex/ex0017/script.js
@@ -2,13 +2,19 @@ function tabuada() {
     let num = document.getElementById('txtn')
     let tabu = document.getElementById('seltab')
 
+    tabu.innerHTML = ''
+
     if ( num.value.length == 0) {
         window.alert('Por favor, digite um número!')
     } else {
         let n = Number(num.value)
         let c = 1
-        for (c = 1; c <= 10; c = n++) {
-            window.alert(`${c}`)
+        for (c = 1; c <= 10; c = c++) {
+            let item = document.createElement('option'); // Cria um elemento <option>
+            item.text = `${n} x ${c} = ${n * c}`; // Define o texto da opção
+            item.value = `tabu${c}`; // Define o valor da opção (opcional, mas útil)
+            tabu.appendChild(item); // Adiciona a opção ao <select>
         }
     }
 }
+
