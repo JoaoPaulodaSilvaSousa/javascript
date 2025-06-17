b216a3586ed87f31229904c67ba65e106f48abe1
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-06-17 20:32:32 -0300

    created saudacao

---
+++ meusexs/saudacao/saudacao.js
@@ -0,0 +1,6 @@
+function seunome() {
+    let nome = document.getElementById('nm').value
+    let resultado = document.getElementById('resul')
+
+    document.getElementById("resul").innerHTML = "Olá, <strong>" + nome + "</strong>!";
+}
