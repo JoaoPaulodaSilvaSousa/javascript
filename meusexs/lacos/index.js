9d65d220914c7a0831e709e4c3255330dd95304d
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-08-04 21:50:47 -0300

    updated javascript

---
+++ meusexs/lacos/index.js
@@ -0,0 +1,26 @@
+// let i = 0;
+// while(i<10) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i = 0;
+// do {
+//     console.log(i)
+//     i++;
+// } while (i<10);
+
+// let resposta = '';
+// do {
+//      resposta = window.prompt('Você é maior de idade?')
+// } while(resposta.toLowerCase() != 'sim');
+
+// for(let i = 0; i<10; i++) {
+//     console.log(i)
+// }
+
+['a', 'b'].forEach(function(item) {
+   console.log(item);
+}) 
+ 
