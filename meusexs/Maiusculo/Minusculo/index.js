074fc5976ea16657e63c0a872274246f8c779d5f
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-09-02 23:27:59 -0300

    Update index.js

--- meusexs/Maiusculo/Minusculo/index.js
+++ meusexs/Maiusculo/Minusculo/index.js
@@ -11,3 +11,17 @@ function minusculo() {
     
     res.innerHTML = palavramin.toLowerCase()
 }
+
+document.getElementById("Palavra1").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+    maiusculo()
+    }
+});
+
+document.getElementById("Palavra2").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+    minusculo()
+    }
+});
